10b17db4c2c93fa60f8a0e0e134a13570cf53fb1
bigbang123789789 <134249895+bigbang123789789@users.noreply.github.com>
2023-10-30 10:25:06 +0700

    Update evaluate.py

--- DocumentUnderstanding/GeoLayoutLM/evaluate.py
+++ DocumentUnderstanding/GeoLayoutLM/evaluate.py
@@ -185,13 +185,14 @@ def visualize_linking(detail_path):
                 link, flag = line.strip('\n').split('\t')
                 fthr_id, son_id = link.split(',')
                 box_father = blk_coord_dict[fthr_id]
-                cv2.rectangle(img, tuple(box_father[:2]), tuple(box_father[2:]), color_box, 2)
+                cv2.rectangle(img, tuple(box_father[:2]), tuple(box_father[2:]), color_box, 3)
                 center_father = ((box_father[0] + box_father[2]) // 2, (box_father[1] + box_father[3]) // 2)
                 box_son = blk_coord_dict[son_id]
-                cv2.rectangle(img, tuple(box_son[:2]), tuple(box_son[2:]), color_box, 2)
+                cv2.rectangle(img, tuple(box_son[:2]), tuple(box_son[2:]), color_box, 3)
                 center_son = ((box_son[0] + box_son[2]) // 2, (box_son[1] + box_son[3]) // 2)
                 # link
-                cv2.arrowedLine(img, center_father, center_son, color_lk[flag], thickness=2, tipLength=0.06)
+                if flag=="RIGHT" or flag=="ERROR":
+                    cv2.arrowedLine(img, center_father, center_son, color_lk["RIGHT"], thickness=3, tipLength=0.06)
         vis_fn = os.path.splitext(os.path.basename(fp))[0] + '.png'
         cv2.imwrite(os.path.join(vis_dir, vis_fn), img)
 
