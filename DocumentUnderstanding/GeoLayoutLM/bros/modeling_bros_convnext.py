d06c70a5906a309051dc489c0b935ee2f3b2efc3
bigbang123789789 <134249895+bigbang123789789@users.noreply.github.com>
2023-11-20 14:13:50 +0700

    Update modeling_bros_convnext.py

--- DocumentUnderstanding/GeoLayoutLM/bros/modeling_bros_convnext.py
+++ DocumentUnderstanding/GeoLayoutLM/bros/modeling_bros_convnext.py
@@ -86,10 +86,7 @@ class GeoLayoutLMModel(BrosPreTrainedModel):
         logger.info(kwargs)
         print("kwargs")
         print(kwargs)
-        logger.info("kwargs line_bbox")
-        logger.info(kwargs["line_bbox"])
-        print("kwargs line_bbox")
-        print(kwargs["line_bbox"])
+
         kwargs["line_bbox"] = bbox
         # ################ get text representation ################
         outputs = self.text_encoder(
